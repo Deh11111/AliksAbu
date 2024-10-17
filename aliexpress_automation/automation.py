61b4ed20e4e418ac3953ed1ff6b568302933095b
Deh11111 <dehter1997@ramber.ru>
2024-10-17 20:35:19 +0300

    Restructe, delete huyna, push pered vihodom na revansh

--- aliexpress_automation/automation.py
+++ aliexpress_automation/automation.py
@@ -1,36 +1,67 @@
 import pyautogui
 import pyperclip
 import time
-from .credentials import get_credentials
+from .credentials import AccountManager
 from .browser_manage import BrowserManager
 
-from .credentials import get_credentials
+# Инициализируем объект класса AccountManager
+manager = AccountManager()
 
 class Automation:
     def __init__(self):
-        self.credentials = get_credentials()  # Получаем учетные данные
+        self.credentials = manager.get_credentials()  # Получаем учетные данные
 
     def open_container_by_index(self, index):
-        """Открывает контейнер по индексу, используя счетчик."""
+
+        # Открывает контейнер по индексу, используя счетчик.
         # Здесь указывайте комбинацию клавиш для открытия контейнера
         pyautogui.hotkey('ctrl', 'shift', str(index))  # Пример открытия нового окна
         time.sleep(1)  # Ждем, чтобы окно открылось
 
-
     def open_containers(self):
-        """Открывает все контейнеры по списку учетных данных."""
+
+        # Открывает все контейнеры по списку учетных данных
         for index in range(len(self.credentials)):
             self.open_container_by_index(index)  # Открываем контейнер по индексу
+            time.sleep(1)
+            self.open_url() # Открываем ссылку
 
         pyautogui.hotkey('alt','1')  # Если это первая вкладка
         time.sleep(1)
         pyautogui.hotkey('ctrl', 'w')  # Закрываем первую вкладку
         time.sleep(1)
-        
+        print("Containers OPENED!")
+
+    def open_url(self):   
+
+        # Переход на страницу "Sign in with Google"
         pyautogui.hotkey('ctrl', 'l')  # Перейти в адресную строку
-        pyautogui.copy("https://accounts.aliexpress.com/user/organization/manage_person_profile.htm?spm=a2g0o.account_setting")  # Вставляем URL
+        time.sleep(1)
+        pyperclip.copy("https://accounts.aliexpress.com/user/organization/manage_person_profile.htm?spm=a2g0o.account_setting")  # Вставляем URL
         time.sleep(1)
         pyautogui.hotkey('ctrl', 'v')
         time.sleep(0.5)
         pyautogui.press('enter')  # Нажимаем Enter
 
+    def open_login_google(self):
+
+        self.open_containers()
+        time.sleep(0.5)
+        self.open_url()
+
+    def check_status_sign_page(self):
+
+        # Проверка не зажевало ли о фроде 
+        pyautogui.hotkey('ctrl', 'l')
+        time.sleep(1)
+        pyautogui.hotkey('ctrl', 'с')
+        time.sleep(1)
+        buffer_text = pyperclip.paste()
+        time.sleep(0.5)
+
+        if "captcha" in buffer_text:
+            print("PIZDA CAPTCHA. No INKOGNITO")
+
+            
+    # def switch_container_by_index(self, index):
+        # pyautogui.hotkey('alt', str(index))
--- aliexpress_automation/credentials.py
+++ aliexpress_automation/credentials.py
@@ -1,6 +1,32 @@
-def get_credentials():
-    """Чтение учетных данных из файла"""
-    with open('credentials.txt', 'r') as file:
-        credentials = file.read().splitlines()
-    return credentials
+class AccountManager:
 
+    def __init__(self, filename='/home/sergey/Desktop/ALIK_SCR/Alex_login_clcker/credentials.txt'):
+        self.filename = filename
+        self.credentials = self.load_credentials()
+
+    def load_credentials(self):
+        # Чтение учетных данных из файла
+        credentials_dict = {}
+        with open(self.filename, 'r') as file:
+            lines = file.read().splitlines()
+            # Начинаем индексацию с 1
+            for idx, line in enumerate(lines, start=1):
+                # Разбиваем строку на email, pass, reserve_email
+                email, password, reserve_email = line.split(':')
+                # Сохраняем данные в словарь с индексом начиная с 1
+                credentials_dict[idx] = {
+                    'email': email,
+                    'password': password,
+                    'reserve_email': reserve_email
+                }
+                
+        return credentials_dict
+
+    def get_credentials(self, index):
+        # Получаем данные по индексу
+        return self.credentials.get(index, None)
+
+# Пример использования:
+manager = AccountManager()
+print(manager.get_credentials(1))  # Теперь выводит данные для первого аккаунта
+print(manager.get_credentials(2))  # Данные для второго аккаунта
